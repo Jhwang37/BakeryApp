e479b98e5c6191652b52a051c1291768d2bd4af1
maxkorzan <maxkorzan@gmail.com>
2020-02-05 16:31:18 -0500

    updating code in max branch

--- src/Bakery.java
+++ src/Bakery.java
@@ -1,74 +1,63 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Bakery {
+    Scanner sc = new Scanner(System.in);
+
     protected String name;
     protected double price;
     protected String expiration;
     protected String diet;
     protected int stock;
-
-    Scanner sc = new Scanner(System.in);
-
-    public ArrayList<String> cookies = new ArrayList<String>();
+    private String dietRestrict = "";
 
 
-    // Cookies c1 = new Cookies("chocolate", 19.11, "2/1999", "soy", 10000);
-
+    //getters and setters
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public double getPrice() {
         return price;
     }
-
     public void setPrice(double price) {
         this.price = price;
     }
-
     public String getExpiration() {
         return expiration;
     }
-
     public void setExpiration(String expiration) {
         this.expiration = expiration;
     }
-
     public String getDiet() {
         return diet;
     }
-
     public void setDiet(String diet) {
         this.diet = diet;
     }
-
     public int getStock() {
         return stock;
     }
-
     public void setStock(int stock) {
         this.stock = stock;
     }
+    public String getDietRestrict() {
+        return dietRestrict;
+    }
+    public void setDietRestrict(String dietRestrict) {
+        this.dietRestrict = dietRestrict;
+    }
 
 
-    public String welcome() {
+    public void welcome() {
         System.out.println("howdy! welcoem to the place");
         System.out.println("We sell the following baked goods: cookies, cake, bread");
-        System.out.println("which are you interested in? ");
-        String category = sc.next();
-
-        return category;
-//
-//        if category.equals("cookies"){
-//            Cookies
-//        } else if
-//    }
-
-
     }
+
+    public void askDiet() {
+            System.out.print("Do you have any diet restrictions (soy, gluten, peanut)? ");
+            dietRestrict = sc.next();
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,101 @@
-import java.util.ArrayList;
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
-        //Bakery welcome();
+        Bakery b = new Bakery();
+        b.welcome();
 
-        Cookies aas = new Cookies();
+        Cookie c1 = new Cookie("choloclate chip", 2.99, "2/20/2020", "gluten", 354);
+        Cookie c2 = new Cookie("oatmeal", 3.99, "3/28/2020", "none", 56);
+        Cookie c3 = new Cookie("sugar", 0.99, "12/31/2020","none",6500);
+        Cookie c4 = new Cookie("snicker doodle",3.50,"5/15/2020","peanut",67);
+        Cookie c5 = new Cookie("gingersnap",1.99,"8/30/2020","none",400);
+
+        c1.askDiet();
+        c1.showCookieList(c1.getDietRestrict());
+        c2.showCookieList(c1.getDietRestrict());
+        c3.showCookieList(c1.getDietRestrict());
+        c4.showCookieList(c1.getDietRestrict());
+        c5.showCookieList(c1.getDietRestrict());
+
 
     }
 
-
 }
+
+
+
+
+
+/////// ******** IGNORE ******** ///////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        //        System.out.println();
+//        System.out.println();
+//        System.out.println(c1.displayCookie());
+//        System.out.println(c2.displayCookie());
+//        System.out.println(c3.displayCookie());
+//        System.out.println(c4.displayCookie());
+//        System.out.println(c5.displayCookie());
+//        System.out.println();
+//        System.out.println();
+
+//        System.out.println("c1.getDiet() = "+c1.getDiet());
+
+
+
+
+
+
+//        ArrayList<String> cookies = new ArrayList<String>();
+//
+//        cookies.add(c1.toString());
+//        cookies.add(c2.toString());
+//        cookies.add(c3.toString());
+//        cookies.add(c4.toString());
+//        cookies.add(c5.toString());
+//
+//        System.out.println(cookies);
+////        for(String key: cookies){
+//        for(int i=0; i<cookies.size(); i++){
+//            System.out.print(cookies.get(i)+ "_");
+//        }
+//
+//        System.out.println();
+//        System.out.println();
+//        System.out.println("cookies.get(1) = "+cookies.get(0));
+//        System.out.println("c1 = "+c1);
+//        System.out.println("c1.toString() = "+c1.toString());
+
+
+
+//        System.out.println(c1.cookies);
+
+        //        Cookies test = new Cookies();
+//        test.setName("testname");
+//        test.setPrice(999);
+//        test.setDiet("soy");
+//        test.setExpiration("march 2020");
+//        test.setStock(1000);
+//
+//        System.out.println("test = "+test);
+//        System.out.println(test.getName() + " " + test.getDiet()+" "+ test.getPrice());
+//
+//        if (test.getDiet().equals("soy")){
+//            System.out.println("this contains soy");
+//        }
+
+
+
+
+//
+//    public static void welcome(){
+//
+//    }
+
+    //        System.out.println("Type: "+c1.getName()+"\t\tPrice: "+c1.getPrice()+"\t\tIn Stock: "+c1.getStock()+"\t\tDiet: " + c1.getDiet()+"\t\tExpiration: "+c1.getExpiration());
+//        System.out.println("Type: "+c2.getName()+"\t\tPrice: "+c2.getPrice()+"\t\tIn Stock: "+c2.getStock()+"\t\tDiet: " + c2.getDiet()+"\t\tExpiration: "+c2.getExpiration());
+//        System.out.println("Type: "+c3.getName()+"\t\tPrice: "+c3.getPrice()+"\t\tIn Stock: "+c3.getStock()+"\t\tDiet: " + c3.getDiet()+"\t\tExpiration: "+c3.getExpiration());
+//        System.out.println("Type: "+c4.getName()+"\t\tPrice: "+c4.getPrice()+"\t\tIn Stock: "+c4.getStock()+"\t\tDiet: " + c4.getDiet()+"\t\tExpiration: "+c4.getExpiration());
+//        System.out.println("Type: "+c5.getName()+"\t\tPrice: "+c5.getPrice()+"\t\tIn Stock: "+c5.getStock()+"\t\tDiet: " + c5.getDiet()+"\t\tExpiration: "+c5.getExpiration());
+
+
+
